767e99f065017be45f737936e68540bcad285fcb
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-01-28 15:21:42 +0900

    first commit

---
+++ grunt/jshint.js
@@ -0,0 +1,10 @@
+module.exports = {
+  options: {
+    browser: true,
+    sub: true,
+    globals: {
+      JST: true
+    },
+  },
+  app: ['dist/assets/js/app.js']
+};
---
+++ tasks/require_config.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {
+  'use strict';
+  var taskName = 'require_config';
+  var description = 'Keep Gruntfile.js simple, stupid';
+
+  var taskFunction = function () {
+  };
+
+  grunt.registerMultiTask(taskName, description, taskFunction);
+};
